2677a62304ec6f14ee97c7def286e8c39ff7d92c
TubaRaye <tubarani76@gmail.com>
2024-12-10 22:41:23 +0500

    Modified

--- StudentNetwork/main.cpp
+++ StudentNetwork/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <windows.h> // Include Windows API
 using namespace std;
+
+
+void setConsoleColor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void clearConsole() {
+    system("cls");
+}
+
+void setCursorPosition(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
+    SetConsoleCursorPosition(hConsole, pos);
+}
+
+void drawBorder(int width, int height) {
+    setConsoleColor(11); // Light cyan
+    for (int i = 0; i <= height; ++i) {
+        for (int j = 0; j <= width; ++j) {
+            if (i == 0 || i == height) {
+                cout << "="; // Top and bottom border
+            } else if (j == 0 || j == width) {
+                cout << "|"; // Left and right border
+            } else {
+                cout << " "; // Empty space inside the border
+            }
+        }
+        cout << endl;
+    }
+    setConsoleColor(7); // Reset to default color
+}
+
+
 void copyString( char source[], char destination[], int& size) {
     int i = 0;
     while (source[i] != '\0') {
@@ -35,9 +71,9 @@ bool checkFriendship(int friendship[][2], int count, int a, int b) {
 void findNHop(int friendships[][2], int friendshipCount, int count,
                      int studentIndex, int N,char studentData[], int studentIndices[]) {
     const int LINESIZE = 1000;
-    int line[LINESIZE][2];  
-    int front = 0, end = 0;      
-    bool visited[100] = {}; 
+    int line[LINESIZE][2];
+    int front = 0, end = 0;
+    bool visited[100] = {};
     line[end][0] = studentIndex;
     line[end][1] = 0;
     end = (end+ 1) % LINESIZE;
@@ -76,13 +112,13 @@ void findNHop(int friendships[][2], int friendshipCount, int count,
     }
 }
 int main() {
-    const int NAMESIZE = 10000;  
-    const int STUDENTS = 100;       
-    const int FRIENDSHIPS = 1000;  
-    char studentData[NAMESIZE];  
-    int studentIndices[STUDENTS];    
-    int friendCounts[STUDENTS];      
-    int friendships[FRIENDSHIPS][2]; 
+    const int NAMESIZE = 10000;
+    const int STUDENTS = 100;
+    const int FRIENDSHIPS = 1000;
+    char studentData[NAMESIZE];
+    int studentIndices[STUDENTS];
+    int friendCounts[STUDENTS];
+    int friendships[FRIENDSHIPS][2];
     int counter = 0, dataIndex = 0, friendshipCounter = 0;
     ifstream fin;
     fin.open("StudentData.csv");
@@ -90,7 +126,7 @@ int main() {
         cout << "file has not been opened." << endl;
     }
     char line[256];
-    fin.getline(line, sizeof(line)); 
+    fin.getline(line, sizeof(line));
     while (fin.getline(line, sizeof(line))) {
         int i = 0, start = 0;
         char studentName[50] = {};
@@ -177,3 +213,42 @@ int main() {
 
     return 0;
 }
+
+/*
+int main() {
+    // Dummy data for demonstration
+    char studentData[] = "Ali Raza\0Fatima Noor\0Ruhma Fatima\0";
+    int studentIndices[] = { 0, 9, 21 };
+    int counter = 3;
+    int friendships[][2] = { {0, 1}, {1, 2} };
+    int friendshipCounter = 2;
+
+    char checkStudent[50] = "Ruhma Fatima";
+    int Index = student_Index(studentData, studentIndices, counter, checkStudent);
+
+    clearConsole();
+    drawBorder(50, 15);
+    setCursorPosition(2, 2);
+    setConsoleColor(14); // Yellow
+    cout << "Student Friendship Explorer";
+
+    if (Index != -1) {
+        setCursorPosition(2, 4);
+        setConsoleColor(10); // Green
+        cout << "Student found: " << checkStudent;
+
+        for (int hop = 1; hop <= 3; hop++) {
+            setCursorPosition(2, 6 + hop);
+            setConsoleColor(9); // Light blue
+            findNHop(friendships, friendshipCounter, counter, Index, hop, studentData, studentIndices);
+        }
+    } else {
+        setCursorPosition(2, 4);
+        setConsoleColor(12); // Red
+        cout << "The mentioned student is not in the dataset.";
+    }
+
+    setConsoleColor(7); // Reset to default color
+    return 0;
+}
+*/
